841a1b160877f341515e3639e7f77d7b4d60c7b3
TakCastel <takcastel@gmail.com>
2019-03-22 09:38:22 +0100

    Refactoring App file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Accueil</router-link> |
+      <router-link to="/contact">Contact</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #282f47;
 }
 #nav {
-  padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #282f47;
     &.router-link-exact-active {
-      color: #42b983;
+      color: #5c6ea7;
     }
   }
 }
