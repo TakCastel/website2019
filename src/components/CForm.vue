8e940ff054c50cb5591e44e48a572f743bfb600d
TakCastel <takcastel@gmail.com>
2019-03-22 09:38:55 +0100

    Contact page

---
+++ src/components/CForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <form>
+    <label for="email"></label>
+    <input name="email" id="email" type="email">
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/views/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="section">
+    <h1>Me contacter</h1>
+    <c-form></c-form>
+  </div>
+</template>
+
+<script>
+import CForm from '@/components/CForm'
+
+export default {
+  name: 'contact',
+
+  components: {
+    CForm
+  }
+}
+</script>
